4b0a8bed82cf0545ffaa6b9284dd55350a8b867a
Arturo Bendezu <artycok123@gmail.com>
2025-07-08 22:13:02 +0200

    feat: Black background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,16 @@ return {
 		lazy = false, --load on startup
 		priority = 1000, --load before other plugins
 		config = function()
+			vim.o.background = "dark"
 			vim.cmd.colorscheme("tokyonight-night") --you can change to 'tokyonight-day' etc.
+
+			vim.cmd([[
+      highlight Normal       guibg=#000000
+      highlight NormalNC     guibg=#000000
+      highlight SignColumn   guibg=#000000
+      highlight VertSplit    guibg=#000000
+      highlight LineNr       guibg=#000000
+    ]])
 		end,
 	},
 }
